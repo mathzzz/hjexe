e0f0b9fea9d4e5c050514def997f86eb8a662202
zhuangjw <zhuangjuwei@163.com>
2021-04-07 18:05:26 +0800

    update

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
+set -x
+[ ! -f /bin/hjexe ] && { sudo cp -flv hjexe /bin/hjexe || exit; }
+md5=$(md5sum.raw /bin/hjexe)
 
 tgt=$(which $1)
-if [ -h $tgt ]; then
-	ls -al $tgt
-	echo do nothing.
-	exit
+tgt2=$tgt
+tgt2=$(readlink.raw -f $tgt)
+tgt_md5=$(md5sum.raw $tgt)
+if [ "$md5" == "$tgt_md5" -a -f $tgt.raw -a "$(md5sum $tgt.raw)" != "$tgt_md5" ]; then
+		ls -al $tgt2 $tgt.raw
+		exit
 fi
 
-if [ -f $tgt.raw ]; then
-	echo $tgt exist.
-	exit
-fi
-
-sudo cp -av $tgt $tgt.raw
-sudo cp -flv /bin/hjexe $tgt
+sudo cp.raw -fav $tgt2 $tgt.raw
+sudo cp.raw -flv /bin/hjexe $tgt2
 touch $HOME/.hj/$1.open
