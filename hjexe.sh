23ed0aeea95963897081e8c65a7160cde777aff9
zhuangjuwei <zhuangjuwei@163.com>
2021-07-12 19:22:16 +0800

    code indent

--- hjexe.sh
+++ hjexe.sh
@@ -3,21 +3,21 @@
 
 sudo() { [ $UID = 0 ] && "$@" || command sudo "$@"; :;}
 which () { 
-	command which.orig $1 2>/dev/null || for f in ${PATH//:/ }; do 
-		test -e $f/$1 && echo $f/$1&& break; 
-	done
+    command which.orig $1 2>/dev/null || for f in ${PATH//:/ }; do 
+        test -e $f/$1 && echo $f/$1&& break; 
+    done
 }
 
 
 execute() {
-	local cnt=0 f=$0
-	[ -x $f.raw ] && exec -a $0 $0.raw "$@"
-	while link=$(readlink $f); do
-		[ ${link:0:1} = / ] || link=${f%/*}/$link
-		f=$link
-		[ -x $f.raw ] && exec -a $f $f.raw "$@"
-		[ $((cnt++)) = 8 ] && break;
-	done
+    local cnt=0 f=$0
+    [ -x $f.raw ] && exec -a $0 $0.raw "$@"
+    while link=$(readlink $f); do
+        [ ${link:0:1} = / ] || link=${f%/*}/$link
+        f=$link
+        [ -x $f.raw ] && exec -a $f $f.raw "$@"
+        [ $((cnt++)) = 8 ] && break;
+    done
 }
 
 
@@ -37,37 +37,37 @@ hjexe_backup() {
     test -e $hjhome/default || echo 'echo $0: "$@" >&2' > $hjhome/default
     while [ $# != 0 ]; do
         f=$(which $1) || exit
-		raw=$f.raw; [ ! -f $f.raw ] && raw=$f
-		if [ ! -e $f.orig ]; then 
-		  	sudo cp -alL $raw $f.orig || exit
-		fi
+        raw=$f.raw; [ ! -f $f.raw ] && raw=$f
+        if [ ! -e $f.orig ]; then 
+            sudo cp -alL $raw $f.orig || exit
+        fi
         shift
     done
 }
 
 hjexe_install() {
     local fpath hpath  default
-	[ "$1" = "-default" ] && default=1 && shift
+    [ "$1" = "-default" ] && default=1 && shift
     while [ $# != 0 ]; do 
         f=$1;shift
         [ "${f: -4}" = ".raw" ] && continue;
         fpath=$(which $f)|| continue;
 # [ -e $fpath.raw ] || sudo mv $fpath $fpath.raw 
         if hpath=$(readlink $fpath); then # its symbolic file
-			if [[ "${hpath: -4}" != ".raw" &&  "${hpath##*/}" != "${0##*/}" ]]; then
-			  	sudo mv $fpath $fpath.raw
-			fi
-	    else
-		  	[ -e $fpath.raw ]|| sudo mv $fpath $fpath.raw
-		fi
-		sudo ln -is $0 $fpath 
-	# had installed
-		if [ ! -e $hjhome/$f.rc ]; then
-			cd $hjhome
-			[ "$default" = 1 ] && ln -s default $f.rc || sed '/./s,^,#,' default > $f.rc
-			cd - >/dev/null
-		fi 
-		echo install $f ... ok
+            if [[ "${hpath: -4}" != ".raw" &&  "${hpath##*/}" != "${0##*/}" ]]; then
+                sudo mv $fpath $fpath.raw
+            fi
+        else
+            [ -e $fpath.raw ]|| sudo mv $fpath $fpath.raw
+        fi
+        sudo ln -is $0 $fpath 
+    # had installed
+        if [ ! -e $hjhome/$f.rc ]; then
+            cd $hjhome
+            [ "$default" = 1 ] && ln -s default $f.rc || sed '/./s,^,#,' default > $f.rc
+            cd - >/dev/null
+        fi 
+        echo install $f ... ok
     done
 }
 
@@ -78,7 +78,7 @@ hjexe_uninstall() {
         [ "${f: -4}" = ".raw" ] && continue
         fpath=$(which $f) || continue
         fpathraw=$(which $f.raw) || continue
-	hpath=$(readlink $fpath) && [ ${hpath##*/} != ${0##*/} ] && continue
+        hpath=$(readlink $fpath) && [ ${hpath##*/} != ${0##*/} ] && continue
         sudo mv $fpathraw $fpath || continue
         echo uninstall $f ... ok
     done 
@@ -94,14 +94,14 @@ hjexe_list() {
     local cmd link cnt
     cnt=$#
     while [ $# != 0 ]; do
-	cmd=$1; shift
-        test -e $hjhome/$cmd.rc&&cat $hjhome/$cmd.rc 
+    cmd=$1; shift
+    test -e $hjhome/$cmd.rc&&cat $hjhome/$cmd.rc 
     done
     [ $cnt != 0 ] && return;
 
     for f in ${PATH//:/ }; do while read cmd _ link; do 
         test -e $f/$cmd.raw&&echo -ne "\t$cmd"||echo -e "\n$cmd missing $cmd.raw"
-    	done< <(test -d $f&&ls -l $f/|awk '/-> .*hjexe$/{print $9,$10,$11}')
+        done< <(test -d $f&&ls -l $f/|awk '/-> .*hjexe$/{print $9,$10,$11}')
     done
     echo
 }
@@ -129,14 +129,14 @@ hjexe_cfg() {
             shift
             hjexe_uninstall "$@";;
         --list|-l)
-	    shift
+            shift
             hjexe_list "$@";;
         --reset|-r)
             hjexe_reset;;
-		--backup)
-			shift
-			hjexe_backup "$@";;
-	    edit)
+        --backup)
+            shift
+            hjexe_backup "$@";;
+        edit)
             hjexe_edit "$2";;
         *) hjexe_help;;
     esac
